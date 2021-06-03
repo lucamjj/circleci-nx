27b03eb88ab45b341e5d9e449cb886287fd19332
Luca <luca.carangella@gmail.com>
2021-06-03 16:15:04 +0100

    add dashboard app

---
+++ apps/dashboard/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div className={styles.page}>
+      <h2>I am dashboard</h2>
+    </div>
+  );
+}
+
+export default Index;
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/circleci-nx'],
+  projects: ['<rootDir>/apps/circleci-nx', '<rootDir>/apps/dashboard'],
 };
