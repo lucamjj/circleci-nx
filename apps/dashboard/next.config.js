664807f1f3763194faa661080fed6e773e822d8f
Luca <luca.carangella@gmail.com>
2021-06-04 09:48:48 +0100

    clean next.config.js

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,12 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require('@nrwl/next/plugins/with-nx');
+// const withNx = require('@nrwl/next/plugins/with-nx');
 
-module.exports = withNx({
-  nx: {
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
+// module.exports = withNx({
+// nx: {
+// Set this to false if you do not want to use SVGR
+// See: https://github.com/gregberge/svgr
+//     svgr: true,
+//   },
+//   target: 'serverless',
+//   distDir: 'dist/apps/dashboard/.next',
+// });
+
+module.exports = {
   target: 'serverless',
   distDir: 'dist/apps/dashboard/.next',
-});
+};
