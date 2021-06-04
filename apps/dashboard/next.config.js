fa26d0a51c9086d5900cc150423201c334b6f7cf
Luca <luca.carangella@gmail.com>
2021-06-04 13:56:17 +0100

    netlify test 8

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -6,6 +6,6 @@ module.exports = withNx({
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
-    target: 'serverless',
   },
+  target: 'serverless',
 });
